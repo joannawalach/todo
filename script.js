7d5d72f0e6cf44c444bc497f92780e9292f37c68
JolaWalach <joanna.walach@op.pl>
2021-10-10 12:09:13 +0200

    add and remove functions added

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-function newTodo() {
-var input = document.getElementById("text");
-    var para = document.createElement("p");
-    para.id = "akapit";
-    var button1 = document.createElement("button");
-    button1.innerText = "usuń";
-    button1.id = "button1";
-    var button2 = document.createElement("button");
-    button2.innerText = "edytuj";
-    button2.id = "button2";
-    para.innerHTML = input.value + button1.outerHTML + button2.outerHTML;
-    document.querySelector(".appbody").appendChild(para);
-    input.value = "";
-
-
-    var button = document.getElementById("button1");
-    button.addEventListener("click", function() {
-        para.parentNode.removeChild(para);
-    })
-
-    var button = document.getElementById("button2");
-    button.addEventListener("click", function() {
-        input.value = para.textContent;
-    })
+let TodoApp = {
+    tasks:[],
+    add: function(name) {
+        this.tasks.push({
+            task: name
+        })
+    },
+    removebyIndex: function(i) {
+        if (i >= this.tasks.length) return;
+        this.tasks.splice(i,0);
+    },
+    printTasks: function() {   
+        for (let i = 0; i <this.tasks.length; i++) {
+        let task = this.tasks[i]
+            console.log(this.task.name[i])
+        }
+        console.log(this.tasks)
+    }
 }
+    
+
+    TodoApp.add("drink water");
+    TodoApp.add("learn Spanish");
+    TodoApp.removebyIndex(3);
+
+
+    TodoApp.printTasks();
