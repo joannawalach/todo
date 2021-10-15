e16229fb607bb90d6cadcf1f5666a96e84dba036
JolaWalach <joanna.walach@op.pl>
2021-10-15 14:49:25 +0200

    limited target div and p

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ let input = document.getElementById("input");
 let newActivity = document.getElementById("input").value;
 let newDiv = document.createElement("p");
 let text = document.createTextNode(newActivity);
-
+if (input==null) {
+    alert("Nie wprowadzono nowego zadania");
+    return;
+} else {
 newDiv.appendChild(text);
+}
 
 let button1 = document.createElement("button");
 newDiv.appendChild(button1);
@@ -26,5 +30,4 @@ button2.addEventListener("click", function() {
 let targetDiv = document.getElementsByTagName("div")[3];
 targetDiv.appendChild(newDiv);
 input.value="";
-
 })
